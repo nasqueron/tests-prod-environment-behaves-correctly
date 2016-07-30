f9b54e77d44ba2c8771eda8f02f4bcac55e93a93
Sébastien Santoro <dereckson@espace-win.org>
2016-07-30 13:55:36 +0000

    Test Auth Grove (login.nasqueron.org)
    Summary:
    The login application contains a /status route returning a 200 "ALIVE"
    to ease monitoring since c494df36.
    
    Basic homepage http/https redirections can be tested too:
      - nginx redirects traffic from http to https with a 301 code
      - the app redirects / to /auth/login when user isn't logged
        in with a 302 code
    
    Finally, during development and deployment, we noticed URL issues.
    We so test the homepage offers links to expected URL, and not
    http://localhost or something else.
    
    Test Plan: Run tests
    
    Reviewers: Sandlayth
    
    Differential Revision: https://devcentral.nasqueron.org/D577

---
+++ AuthGroveTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'traits/assertHttp.php';
+
+class AuthGroveTest extends PHPUnit_Framework_TestCase {
+    use assertHttp;
+
+    public function testRedirections () {
+        $this->assertHttpResponseCode(301, 'http://login.nasqueron.org', 'Webserver should be configured to redirect http to https.');
+        $this->assertHttpResponseCode(302, 'https://login.nasqueron.org', '/ should redirect to login page');
+        $this->assertHttpResponseCode(404, 'https://login.nasqueron.org/notexisting', 'A 404 code were expected for a not existing page.');
+    }
+
+    public function testSSL () {
+        $this->assertHttpResponseCode(200, 'https://login.nasqueron.org/auth/login', "Auth Grove HTTPS login page isn't reachable.");
+    }
+
+    public function testAlive () {
+        $url = 'https://login.nasqueron.org/status';
+        $this->assertHttpResponseCode(200, $url);
+        $this->assertSame('ALIVE', file_get_contents($url));
+    }
+
+    public function testHomepage () {
+        $content = file_get_contents('https://login.nasqueron.org');
+        $this->assertContains('https://login.nasqueron.org/auth/', $content);
+    }
+}
