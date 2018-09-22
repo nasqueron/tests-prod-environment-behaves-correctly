057410c9e813c1b607a35327683e4033000d2e86
Sébastien Santoro <dereckson@espace-win.org>
2018-09-22 23:23:49 +0200

    Switch PHPDoc comments to PHP 7 methods signatures types
    Test Plan: Run all tests with `make`
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D1816

--- traits/assertHttp.php
+++ traits/assertHttp.php
@@ -3,22 +3,16 @@
 trait assertHttp {
 	/**
 	 * Asserts the HTTP response code of an URL matches the expected code
-	 *
-	 * @param int $expectedCode the expected HTTP response code
-	 * @param string $url the URL to check
-	 * @param string $comment the comment to output if the test fails [facultative]
 	 */
-	private function assertHttpResponseCode ($expectedCode, $url, $comment = '') : void {
+	private function assertHttpResponseCode (int $expectedCode, string $url, string $comment = '') : void {
 		$actualCode = $this->getHttpResponseCode($url);
 		$this->assertEquals($expectedCode, $actualCode, $comment);
 	}
 
 	/**
 	 * Gets the HTTP response code of the specified URL
-	 *
-	 * @param string $url
 	 */
-	private function getHttpResponseCode ($url) : int {
+	private function getHttpResponseCode (string $url) : int {
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_USERAGENT, "Nasqueron-Ops-Tests");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
