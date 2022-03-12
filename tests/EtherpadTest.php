2a46d7c6e5c39d526ae0ddb2339dd44aefc21779
Sébastien Santoro <dereckson@espace-win.org>
2022-03-12 14:40:51 +0000

    Drop support for ether-o-meter Etherpad plugin
    Summary: Ref T1700
    
    Test Plan: `phpunit EtherpadTest.php`
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Maniphest Tasks: T1700
    
    Differential Revision: https://devcentral.nasqueron.org/D2587

--- tests/EtherpadTest.php
+++ tests/EtherpadTest.php
@@ -12,7 +12,10 @@ class EtherpadTest extends TestCase {
         $this->assertHttpResponseCode(200, 'https://pad.nasqueron.org/', "Etherpad looks down.");
         $this->assertHttpResponseCode(200, 'https://pad.wolfplex.be', "Etherpad doesn't reply to pad.wolfplex.be vhost.");
         $this->assertHttpResponseCode(404, 'https://pad.nasqueron.org/notexisting', 'A 404 code were expected for a not existing Etherpad page.');
-        $this->assertHttpResponseCode(200, 'https://pad.nasqueron.org/metrics', "ep_ether-o-meter plugin doesn't seem installed.");
+    }
+
+    public function testDeprecatedPluginAreNotInstalled () {
+        $this->assertHttpResponseCode(404, 'https://pad.nasqueron.org/metrics', "ep_ether-o-meter plugin doesn't seem installed.");
     }
 
     public function testWolfplexApiWorks () {
